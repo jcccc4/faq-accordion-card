6b2c131175f6878b3f90b53349b6a2420756bec6
JC <jcsacra4@gmail.com>
2023-06-18 16:02:01 +0800

    Added a desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,16 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily:{
-        ks:[ 'Kumbh Sans', 'sans-serif']
+      fontFamily: {
+        ks: ["Kumbh Sans", "sans-serif"],
       },
-      fontSize:{
-        'faq':['32px','40px'],
-        question:['13px','16px']
-      }
+      fontSize: {
+        faq: ["32px", "40px"],
+        question: ["13px", "16px"],
+        'question-lg': ["14px", "17px"],
+        answers: ["12px", "18px"],
+      },
     },
   },
   plugins: [],
-}
+};
